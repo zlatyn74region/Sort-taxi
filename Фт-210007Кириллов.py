46d972474e38ad7911804cc98bf5e9e5a704d85a
zlatyn74region <zlatyn74region@mail.ru>
2022-12-09 15:09:05 +0500

    Add comments

--- Фт-210007Кириллов.py
+++ Фт-210007Кириллов.py
@@ -1,10 +1,24 @@
+# Используем дополнительную библиотеку для 
+# преобразования чисел в словесную форму.
 from library.Lab2 import num2text
+
 
 def input_int(
     msg: str, 
     min: int = None, 
     max: int = None,
 ) -> int:
+    '''
+    Берет на ввод у пользователя число с дальнейшей проверкой.
+
+    Параметры:
+    msg - Сообщение подающееся на ввод пользователю.
+    min - Минимальное значение на ввод.
+    max - Максимальное значение на ввод.
+
+    Возврат:
+    Корректно введенное число.
+    '''
     while True:
         try:
             num = int(input(msg))
@@ -17,25 +31,29 @@ def input_int(
         except:
             print('Ошибка: нужно ввести число!')
 
+
 amount_emps = input_int('Введите количество сотрудников: ', 1, 1000)
 
-kils = list()
+kiloms = list()
 for i in range(amount_emps):
-    kils.append(input_int(f'Введите количество километров до дома для {i+1} сотрудника: ', 1))
+    kiloms.append(input_int(f'Введите количество километров до дома для {i+1} сотрудника: ', 1))
 
 tax = list()
 for i in range(amount_emps):
     tax.append(input_int(f'Введите стоимость тарифа в рублях за проезд одного километра для {i+1} машины такси: ', 1))
 
+# Список, в котором каждому сотруднику соотвествует его такси.
 final = [0 for i in range(amount_emps)]
 
+# Подсчет суммы и распределение сотрудников по такси.
 sum = 0
 for i in range(amount_emps):
-    sum += min(kils) * max(tax)
-    final[kils.index(min(kils))] = tax.index(max(tax))
-    kils[kils.index(min(kils))] = max(kils)+1
+    sum += min(kiloms) * max(tax)
+    final[kiloms.index(min(kiloms))] = tax.index(max(tax))
+    kiloms[kiloms.index(min(kiloms))] = max(kiloms)+1
     tax[tax.index(max(tax))] = 0
 
+# Вывод результатов.
 for i in range(amount_emps):
     print(f'Для {i+1} сотрудника {final[i]+1} такси ')
 
